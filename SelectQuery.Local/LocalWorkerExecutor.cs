68c00ef6d90471d0de62b70329384a3b2ecff531
Will Smith <will@toxon.co.uk>
2019-10-06 15:32:13 +0100

    Add program for local testing

---
+++ SelectQuery.Local/LocalWorkerExecutor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SelectQuery.Distribution;
+using SelectQuery.Results;
+using SelectQuery.Workers;
+
+namespace SelectQuery.Local
+{
+    public class LocalWorkerExecutor : IWorkerExecutor
+    {
+        private readonly Worker _worker;
+
+        public LocalWorkerExecutor(Worker worker)
+        {
+            _worker = worker;
+        }
+
+        public async Task<IReadOnlyCollection<Result>> ExecuteAsync(DistributorPlan plan, IReadOnlyList<Uri> sources)
+        {
+            var tasks = new Task<Result>[sources.Count];
+
+            for (var i = 0; i < sources.Count; i++)
+            {
+                var source = sources[i];
+
+                tasks[i] = _worker.QueryAsync(new WorkerInput(plan.WorkerPlan, source));
+            }
+
+            return await Task.WhenAll(tasks);
+        }
+    }
+}
