0b328e7cc405b23f8c4e0fd331e1fbd4591e6c0f
Will Smith <will@toxon.co.uk>
2022-03-30 21:26:47 +0100

    Implement evaluating aggregate functions

--- SelectQuery.Evaluation/JsonLinesEvaluator.cs
+++ SelectQuery.Evaluation/JsonLinesEvaluator.cs
@@ -1,4 +1,6 @@
-﻿using SelectParser.Queries;
+﻿using OneOf.Types;
+using SelectParser;
+using SelectParser.Queries;
 using Utf8Json;
 using Utf8Json.Resolvers;
 
@@ -27,29 +29,75 @@ namespace SelectQuery.Evaluation
             var reader = new JsonReader(file);
             var writer = new JsonWriter();
 
-            while (ProcessRecord(ref reader, ref writer)) { }
+            if (QueryValidator.IsAggregateQuery(_query))
+            {
+                ProcessAggregate(ref reader, ref writer);
+            }
+            else
+            {
+                while (ProcessRecord(ref reader, ref writer)) { }
+            }
 
             return writer.ToUtf8ByteArray();
         }
         
+        private void ProcessAggregate(ref JsonReader reader, ref JsonWriter writer)
+        {
+            var state = new AggregateProcessor(_query);
+
+            while (true)
+            {
+                var readResult = ReadRecord(ref reader);
+                if (readResult.IsNone)
+                {
+                    break;
+                }
+
+                var record = readResult.AsT0;
+                if (TestPredicate(record))
+                {
+                    state.ProcessRecord(record);
+                }
+            }
+
+            state.Write(ref writer);
+            writer.WriteRaw((byte) '\n');
+        }
+        
         private bool ProcessRecord(ref JsonReader reader, ref JsonWriter writer)
         {
-            reader.SkipWhiteSpace();
-            if (reader.GetCurrentOffsetUnsafe() >= reader.GetBufferUnsafe().Length)
+            var readResult = ReadRecord(ref reader);
+            if (readResult.IsNone)
             {
                 return false;
             }
 
-            var obj = Formatter.Deserialize(ref reader, StandardResolver.Default);
-
-            var wherePassed = _query.Where.Match(where => ExpressionEvaluator.EvaluateOnTable<bool>(where.Condition, _query.From, obj), _ => true);
-            if (wherePassed.IsSome && wherePassed.AsT0)
+            var record = readResult.AsT0;
+            if (TestPredicate(record))
             {
-                _recordWriter.Write(ref writer, obj);
+                _recordWriter.Write(ref writer, record);
                 writer.WriteRaw((byte) '\n');
             }
 
             return true;
+        }
+
+        private Option<object> ReadRecord(ref JsonReader reader)
+        {
+            reader.SkipWhiteSpace();
+            if (reader.GetCurrentOffsetUnsafe() >= reader.GetBufferUnsafe().Length)
+            {
+                return new None();
+            }
+
+            return Formatter.Deserialize(ref reader, StandardResolver.Default);
+        }
+
+        private bool TestPredicate(object record)
+        {
+            var wherePassed = _query.Where.Match(where => ExpressionEvaluator.EvaluateOnTable<bool>(where.Condition, _query.From, record), _ => true);
+
+            return wherePassed.IsSome && wherePassed.AsT0;
         }
     }
 }
