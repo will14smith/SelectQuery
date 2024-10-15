2d55c73fae06e44dc5e3f172b0cf3ad356677280
Will Smith <will@toxon.co.uk>
2024-10-15 21:14:12 +0100

    Make option lighter weight

--- SelectParser/Option.cs
+++ SelectParser/Option.cs
@@ -1,27 +1,47 @@
 ﻿using System;
-using OneOf;
+using System.Collections.Generic;
 using OneOf.Types;
 
 namespace SelectParser;
 
-public class Option<T> : OneOfBase<T, None>
+public readonly struct Option<T> : IEquatable<Option<T>>
 {
-    private Option() : base(new None()) { }
-    private Option(T value) : base(value) { }
+    public Option(T value)
+    {
+        HasValue = true;
+        Value = value;
+    }
+    public Option()
+    {
+        HasValue = false;
+        Value = default;
+    }
 
+    public bool HasValue { get; }
+    public T? Value { get; }
+    public T AsT0 => HasValue ? Value! : throw new InvalidOperationException();
+    
+    public bool IsSome => HasValue;
+    public bool IsNone => !HasValue;
+    
     public static implicit operator Option<T>(T t) => new(t);
     public static implicit operator Option<T>(None _) => new();
-
-    public bool IsSome => IsT0;
-    public bool IsNone => IsT1;
-
-    public Option<TOut> Select<TOut>(Func<T, TOut> mapFn)
+    
+    public bool Equals(Option<T> other) => HasValue == other.HasValue && EqualityComparer<T?>.Default.Equals(Value, other.Value);
+    public override bool Equals(object? obj) => obj is Option<T> other && Equals(other);
+    public override int GetHashCode()
     {
-        return Match(val => new Option<TOut>(mapFn(val)), none => none);
+        unchecked
+        {
+            return (HasValue.GetHashCode() * 397) ^ EqualityComparer<T?>.Default.GetHashCode(Value);
+        }
     }
 
-    public Option<TOut> SelectMany<TOut>(Func<T, Option<TOut>> mapFn)
-    {
-        return Match(mapFn, none => none);
-    }
+    public static bool operator ==(Option<T> left, Option<T> right) => left.Equals(right);
+    public static bool operator !=(Option<T> left, Option<T> right) => !left.Equals(right);
+
+    public Option<TOut> Select<TOut>(Func<T, TOut> mapFn) => HasValue ? new Option<TOut>(mapFn(Value!)) : new Option<TOut>();
+    public Option<TOut> SelectMany<TOut>(Func<T, Option<TOut>> mapFn) => HasValue ? mapFn(Value!) : new Option<TOut>();
+
+    public TResult Match<TResult>(Func<T, TResult> someFn, Func<T?, TResult> noneFn) => HasValue ? someFn(Value!) : noneFn(Value);
 }
--- SelectParser/Queries/Expression.cs
+++ SelectParser/Queries/Expression.cs
@@ -482,7 +482,7 @@ public partial class Expression : OneOfBase<Expression.StringLiteral, Expression
             {
                 var hashCode = Expression?.GetHashCode() ?? 0;
                 hashCode = (hashCode * 397) ^ (Pattern?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Escape?.GetHashCode() ?? 0);
+                hashCode = (hashCode * 397) ^ Escape.GetHashCode();
                 return hashCode;
             }
         }
